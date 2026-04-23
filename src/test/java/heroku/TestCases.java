f88216136758b69df9766bb966d0e69e83607e29
hvietnguyen88 <hviet.nguyen88@gmail.com>
2026-04-23 15:22:35 +0300

    Add TestCases.java with login test and fix browser consistency (Chrome)

--- src/test/java/heroku/TestCases.java
+++ src/test/java/heroku/TestCases.java
@@ -2,18 +2,18 @@ package heroku;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestCases {
     @Test
     public void loginWithValidCredentials() {
-        EdgeOptions edgeOptions = new EdgeOptions();
-        edgeOptions.addArguments("--headless=new");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless=new");
 
-        WebDriver driver = new EdgeDriver(edgeOptions);
+        WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://the-internet.herokuapp.com/login");
 
         driver.findElement(By.id("username")).sendKeys("tomsmith");
