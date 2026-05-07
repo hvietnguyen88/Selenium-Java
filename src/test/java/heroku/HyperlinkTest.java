d2a453906b610614d6b2d116f90c5266efa0d47c
vietnguyen <hviet.nguyen88@gmail.com>
2026-05-07 22:21:50 +0300

    Commit TestCases

--- src/test/java/heroku/HyperlinkTest.java
+++ src/test/java/heroku/HyperlinkTest.java
@@ -2,29 +2,32 @@ package heroku;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HyperlinkTest {
     @Test
-    void successfullyAccessHyperlinks() throws InterruptedException {
-        WebDriver driver = new EdgeDriver();
+    void successfullyAccessHyperlinks(){
+        WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/status_codes");
 
         driver.findElement(By.linkText("200")).click();
         Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/status_codes/200");
-        Thread.sleep(1000);
         driver.findElement(By.linkText("here")).click();
 
         driver.findElement(By.linkText("301")).click();
         Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/status_codes/301");
-        Thread.sleep(1000);
         driver.findElement(By.linkText("here")).click();
 
         driver.findElement(By.linkText("404")).click();
         Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/status_codes/404");
-        Thread.sleep(1000);
+        driver.findElement(By.linkText("here")).click();
 
+        driver.findElement(By.linkText("500")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/status_codes/500");
+        driver.findElement(By.linkText("here")).click();
+
+        driver.quit();
     }
 }
